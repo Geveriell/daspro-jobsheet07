c989be1ffb19c05046af9e1ddea88bfbdec96c7f
Geveriell <erzaardhyaputra@gmail.com>
2025-10-22 11:41:48 +0700

    Modif percobaan 2.2

--- SiakadFor07.java
+++ SiakadFor07.java
@@ -1,16 +1,18 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
-public class SiakadFor07 { 
-    public static void main(String[] args) { 
-        Scanner sc = new Scanner(System.in); 
+public class SiakadFor07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        double nilai, tertinggi = 0, terendah = 100; // Deklarasi variabel
+        double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0; // Tambahan variabel
 
         // perulangan for 1-10
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + " : ");
             nilai = sc.nextDouble();
 
+            // cek nilai tertinggi dan terendah
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
@@ -18,12 +20,21 @@ public class SiakadFor07 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+
+            // cek kelulusan
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
 
         // output
         System.out.println("\nNilai tertinggi = " + tertinggi);
         System.out.println("Nilai terendah  = " + terendah);
+        System.out.println("Jumlah mahasiswa lulus     = " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus = " + tidakLulus);
 
-        sc.close(); 
+        sc.close();
     }
 }
